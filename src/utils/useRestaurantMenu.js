c88916675f3efd826a13ff43892b66c438209202
Taniya Yadav <128471154+Taniya23Y@users.noreply.github.com>
2024-08-30 06:08:26 +0000

    handled: Failed to fetch

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -6,14 +6,30 @@ const useRestaurantMenu = (resId) => {
   // fetch the data
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [resId]);
+
+  // const fetchData = async () => {
+  //   const data = await fetch(Menu_API + resId);
+  //   console.log(Menu_API + resId);
+
+  //   const json = await data.json();
+  //   console.log(json);
+  //   setResInfo(json);
+  // };
 
   const fetchData = async () => {
-    const data = await fetch(Menu_API + resId);
-    const json = await data.json();
-    // console.log(json);
-    setResInfo(json);
+    try {
+      const data = await fetch(Menu_API + resId);
+      if (!data.ok) {
+        throw new Error(`HTTP error! status: ${data.status}`);
+      }
+      const json = await data.json();
+      setResInfo(json);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    }
   };
+  
 
   return resInfo;
 };
